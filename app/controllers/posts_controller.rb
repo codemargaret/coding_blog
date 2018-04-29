7ce2c1ae31c9d3f1e6a78b9af44455ba78e3b9a7
Margaret Berry <codeberry1@gmail.com>
2018-04-28 21:24:05 -0700

    add create, update, and destroy methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,32 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.order("created_at DESC")
     render json: @posts
+  end
+
+  def create
+    @post = Post.create(post_params)
+    render json: @post
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    render json: @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      head :no_content, status: :ok
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :date)
   end
 end
